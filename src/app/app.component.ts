d0c373f645cbbb3f01fab42eb962aa8ecc8f3af7
Jose Carrasco <jscarrasco18@outlook.com>
2024-09-20 21:40:15 -0400

    [20-09-2024]. interface js operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface IPerson{
+  name: string;
+  lastName: string;
+  age: number;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,12 +19,24 @@ export class AppComponent {
   public numberToSum:number = 11;
   public numberOne:number = 12;
   public numberTwo:number = 10;
-
-
+  public animals:string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  public person: IPerson = {
+    name: 'Jose',
+    lastName: 'Carrasco',
+    age: 21
+  };
 
   constructor(){
     console.log('SUM: ', this.sum(this.numberOne, this.numberTwo));
     console.log('SUBSTRACT: ', this.substract(this.numberOne, this.numberTwo));
+
+    console.log('MAP: ', this.animals.map((animal) => animal+'new'));
+    console.log('FOREACH: ', this.animals.forEach((animal) => animal+'new'));
+    console.log('FIND: ', this.animals.find((animal) => animal === 'f'));
+    console.log('FILTER: ', this.animals.filter((animal) => animal === 'c'));
+    console.log('INDEXOF: ', this.animals.indexOf('d'));
+    console.log('FINDINDEX: ', this.animals.findIndex((animal) => animal === 'c'));
+    console.log('REDUCE: ', this.animals.reduce((accumulatedValue, arrayValue) => accumulatedValue + arrayValue, ''));
   }
 
   public sum(num1:number, num2: number): number{
@@ -31,8 +49,8 @@ export class AppComponent {
 
   public getArray(): void{
     const persons: number[] = [1, 2, 3, 4, 5].filter(person => person % 2 === 0);
-    for (let index = 0; index < persons.length; index++) {
-      console.log('person = ', persons[index]);
+    for (let person of persons) {
+      console.log('person = ', person);
     }
   }
 }
