0146a05bb328525a2fbda0725f6c41cabc20cf6a
Jose Carrasco <jscarrasco18@outlook.com>
2024-09-20 20:30:19 -0400

    [20-09-2024]. using public and private functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,19 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   public title:string = 'certification.upb.web';
   public numberToSum:number = 11;
+  public numberOne:number = 12;
+  public numberTwo:number = 10;
+
+  constructor(){
+    console.log('SUM: ', this.sum(this.numberOne, this.numberTwo));
+    console.log('SUBSTRACT: ', this.substract(this.numberOne, this.numberTwo));
+  }
+
+  public sum(num1:number, num2: number): number{
+    return num1 + num2;
+  }
+
+  private substract(num1:number, num2: number): number{
+    return num1 - num2;
+  }
 }
