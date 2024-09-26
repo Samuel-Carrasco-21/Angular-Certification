6159982ba91e1202145da051b10d628523e16884
Jose Carrasco <jscarrasco18@outlook.com>
2024-09-25 20:37:06 -0400

    [25-09-2024]. destructure object & spreed, rest operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,24 +19,41 @@ export class AppComponent {
   public numberToSum:number = 11;
   public numberOne:number = 12;
   public numberTwo:number = 10;
+
   public animals:string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-  public person: IPerson = {
+
+  private person: IPerson = {
     name: 'Jose',
     lastName: 'Carrasco',
     age: 21
   };
 
-  constructor(){
-    console.log('SUM: ', this.sum(this.numberOne, this.numberTwo));
-    console.log('SUBSTRACT: ', this.substract(this.numberOne, this.numberTwo));
+  private students:number[] = [1, 2, 3, 4, 5, 6];
+  private parents:number[] = [7, 8, 9, 10];
 
-    console.log('MAP: ', this.animals.map((animal) => animal+'new'));
-    console.log('FOREACH: ', this.animals.forEach((animal) => animal+'new'));
-    console.log('FIND: ', this.animals.find((animal) => animal === 'f'));
-    console.log('FILTER: ', this.animals.filter((animal) => animal === 'c'));
-    console.log('INDEXOF: ', this.animals.indexOf('d'));
-    console.log('FINDINDEX: ', this.animals.findIndex((animal) => animal === 'c'));
-    console.log('REDUCE: ', this.animals.reduce((accumulatedValue, arrayValue) => accumulatedValue + arrayValue, ''));
+  constructor(){
+    const { name, age } = this.person;
+    console.log('Destructuring object: { name:', name, ', age:', age, '}');
+
+    const students_and_parents:number[] = [...this.students, ...this.parents];
+    console.log('Spreed Operator: ', students_and_parents);
+
+    console.log('Sum of many numbers (Rest Operator): ', this.sumManyNumbers(10, ...students_and_parents));
+
+    // console.log('--- Array Operators ---');
+    // console.log('SUM: ', this.sum(this.numberOne, this.numberTwo));
+    // console.log('SUBSTRACT: ', this.substract(this.numberOne, this.numberTwo));
+    // console.log('MAP: ', this.animals.map((animal) => animal+'new'));
+    // console.log('FOREACH: ', this.animals.forEach((animal) => animal+'new'));
+    // console.log('FIND: ', this.animals.find((animal) => animal === 'f'));
+    // console.log('FILTER: ', this.animals.filter((animal) => animal === 'c'));
+    // console.log('INDEXOF: ', this.animals.indexOf('d'));
+    // console.log('FINDINDEX: ', this.animals.findIndex((animal) => animal === 'c'));
+    // console.log('REDUCE: ', this.animals.reduce((accumulatedValue, arrayValue) => accumulatedValue + arrayValue, ''));
+  }
+
+  public sumManyNumbers(initialValue: number, ...nums:number[]): number{
+    return nums.reduce((a, b) => a + b, initialValue);
   }
 
   public sum(num1:number, num2: number): number{
